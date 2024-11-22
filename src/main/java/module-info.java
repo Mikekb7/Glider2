3f7d459e0788815effc0b168e10edb7b7dccb594
Michael Bonger <michaelbonger831@gmail.com>
2024-11-22 12:10:21 -0500

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.glider2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.glider2 to javafx.fxml;
+    exports org.example.glider2;
+}
