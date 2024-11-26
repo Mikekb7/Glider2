6551e04ad72645e252d3a8f73d0ed825ca976d44
Michael Bonger <michaelbonger831@gmail.com>
2024-11-25 23:42:08 -0500

    Establishing connection to Database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.example.glider2 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens org.example.glider2 to javafx.fxml;
     exports org.example.glider2;
---
+++ src/main/java/org/example/glider2/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package org.example.glider2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://<gliderserver.mysql.database.azure.com>:<3306>/<gliderdatabase>", "glider", "Gpassword123");
+            System.out.println("Successful!");
+        } catch(SQLException e){
+            System.out.println("Connection failed.");
+
+        }
+    }
+}
