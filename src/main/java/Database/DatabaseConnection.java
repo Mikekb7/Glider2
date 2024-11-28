584524319fea7acdf8f815b90d6ff96aded70a60
Michael Bonger <michaelbonger831@gmail.com>
2024-11-28 13:05:33 -0500

    Database part2

--- src/main/java/Database/DatabaseConnection.java
+++ src/main/java/Database/DatabaseConnection.java
@@ -1,19 +1,26 @@
 package Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
 
 public class DatabaseConnection {
     public static void main(String[] args) {
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://<gliderserver.mysql.database.azure.com>:<3306>/<gliderdatabase>", "glider", "Gpassword123");
+            connection = DriverManager.getConnection("jdbc:mysql://gliderserver.mysql.database.azure.com:3306/gliderdatabase?useSSL=true", "glider", "Gpassword123");
             System.out.println("Successful!");
+
+            PreparedStatement statement = connection.prepareStatement(Queries.login);
+
+
         } catch(SQLException e){
             System.out.println("Connection failed.");
 
+        } finally{
+            connection.close();
         }
-        System.out.println("Database Connected");
     }
 }
