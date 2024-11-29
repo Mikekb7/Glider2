4f94063cbe6dc8932d35b97cfad4b6d7dff1a99d
Michael Bonger <michaelbonger831@gmail.com>
2024-11-29 12:27:39 -0500

    Customer and flights class work

---
+++ src/main/java/Flights.java
@@ -0,0 +1,41 @@
+public class Flights {
+    private String flightNumber;
+    private String departure;
+    private String destination;
+    private String airline;
+
+    public Flights(String flightNumber, String departure, String destination, String airline){
+        this.flightNumber = flightNumber;
+        this.departure = departure;
+        this.destination = destination;
+        this.airline = airline;
+    }
+
+    public String getFlightNumber(){
+        return flightNumber;
+    }
+    public void setFlightNumber(String flightNumber){
+        this.flightNumber = flightNumber;
+    }
+    public String getDeparture(){
+        return departure;
+    }
+    public void setDeparture(String departure){
+        this.departure = departure;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+
+    public String getAirlines(){
+        return airline;
+    }
+    public void setAirlines(String airline){
+        this.airline = airline;
+    }
+}
